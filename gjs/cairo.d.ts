1e5bc476433dfde1508502957a16727906fab6d0
Pascal Garber <pascal@artandcode.studio>
2024-07-24 19:16:35 +0200

    Bump version 4.0.0-beta.12

--- gjs/cairo.d.ts
+++ gjs/cairo.d.ts
@@ -1,5 +1,6 @@
 // Cairo 1.0
 import type Cairo from '@girs/cairo-1.0';
+import type GObject from '@girs/gobject-2.0';
 
 declare namespace giCairo {
     // Add overrides here
@@ -10,28 +11,333 @@ declare namespace giCairo {
     // - https://gitlab.gnome.org/GNOME/gjs/-/blob/master/modules/cairo-context.cpp
 
     // Re-exported enums
-    export type Status = Cairo.Status;
-    export type Content = Cairo.Content;
-    export type Operator = Cairo.Operator;
-    export type Antialias = Cairo.Antialias;
-    export type FillRule = Cairo.FillRule;
-    export type LineCap = Cairo.LineCap;
-    export type LineJoin = Cairo.LineJoin;
-    export type TextClusterFlags = Cairo.TextClusterFlags;
-    export type FontSlant = Cairo.FontSlant;
-    export type FontWeight = Cairo.FontWeight;
-    export type SubpixelOrder = Cairo.SubpixelOrder;
-    export type HintStyle = Cairo.HintStyle;
-    export type HintMetrics = Cairo.HintMetrics;
-    export type FontType = Cairo.FontType;
-    export type PathDataType = Cairo.PathDataType;
-    export type DeviceType = Cairo.DeviceType;
-    export type SurfaceType = Cairo.SurfaceType;
-    export type Format = Cairo.Format;
-    export type PatternType = Cairo.PatternType;
-    export type Extend = Cairo.Extend;
-    export type Filter = Cairo.Filter;
-    export type RegionOverlap = Cairo.RegionOverlap;
+    export namespace Status {
+        export const $gtype: GObject.GType<Status>;
+    }
+
+    enum Status {
+        SUCCESS,
+        NO_MEMORY,
+        INVALID_RESTORE,
+        INVALID_POP_GROUP,
+        NO_CURRENT_POINT,
+        INVALID_MATRIX,
+        INVALID_STATUS,
+        NULL_POINTER,
+        INVALID_STRING,
+        INVALID_PATH_DATA,
+        READ_ERROR,
+        WRITE_ERROR,
+        SURFACE_FINISHED,
+        SURFACE_TYPE_MISMATCH,
+        PATTERN_TYPE_MISMATCH,
+        INVALID_CONTENT,
+        INVALID_FORMAT,
+        INVALID_VISUAL,
+        FILE_NOT_FOUND,
+        INVALID_DASH,
+        INVALID_DSC_COMMENT,
+        INVALID_INDEX,
+        CLIP_NOT_REPRESENTABLE,
+        TEMP_FILE_ERROR,
+        INVALID_STRIDE,
+        FONT_TYPE_MISMATCH,
+        USER_FONT_IMMUTABLE,
+        USER_FONT_ERROR,
+        NEGATIVE_COUNT,
+        INVALID_CLUSTERS,
+        INVALID_SLANT,
+        INVALID_WEIGHT,
+        INVALID_SIZE,
+        USER_FONT_NOT_IMPLEMENTED,
+        DEVICE_TYPE_MISMATCH,
+        DEVICE_ERROR,
+        INVALID_MESH_CONSTRUCTION,
+        DEVICE_FINISHED,
+        JBIG2_GLOBAL_MISSING,
+    }
+
+    export namespace Content {
+        export const $gtype: GObject.GType<Content>;
+    }
+
+    enum Content {
+        COLOR,
+        ALPHA,
+        COLOR_ALPHA,
+    }
+
+    export namespace Operator {
+        export const $gtype: GObject.GType<Operator>;
+    }
+
+    enum Operator {
+        CLEAR,
+        SOURCE,
+        OVER,
+        IN,
+        OUT,
+        ATOP,
+        DEST,
+        DEST_OVER,
+        DEST_IN,
+        DEST_OUT,
+        DEST_ATOP,
+        XOR,
+        ADD,
+        SATURATE,
+        MULTIPLY,
+        SCREEN,
+        OVERLAY,
+        DARKEN,
+        LIGHTEN,
+        COLOR_DODGE,
+        COLOR_BURN,
+        HARD_LIGHT,
+        SOFT_LIGHT,
+        DIFFERENCE,
+        EXCLUSION,
+        HSL_HUE,
+        HSL_SATURATION,
+        HSL_COLOR,
+        HSL_LUMINOSITY,
+    }
+
+    export namespace Antialias {
+        export const $gtype: GObject.GType<Antialias>;
+    }
+
+    enum Antialias {
+        DEFAULT,
+        NONE,
+        GRAY,
+        SUBPIXEL,
+        FAST,
+        GOOD,
+        BEST,
+    }
+
+    export namespace FillRule {
+        export const $gtype: GObject.GType<FillRule>;
+    }
+
+    enum FillRule {
+        WINDING,
+        EVEN_ODD,
+    }
+
+    export namespace LineCap {
+        export const $gtype: GObject.GType<LineCap>;
+    }
+
+    enum LineCap {
+        BUTT,
+        ROUND,
+        SQUARE,
+    }
+
+    export namespace LineJoin {
+        export const $gtype: GObject.GType<LineJoin>;
+    }
+
+    enum LineJoin {
+        MITER,
+        ROUND,
+        BEVEL,
+    }
+
+    export namespace TextClusterFlags {
+        export const $gtype: GObject.GType<TextClusterFlags>;
+    }
+
+    enum TextClusterFlags {
+        BACKWARD,
+    }
+
+    export namespace FontSlant {
+        export const $gtype: GObject.GType<FontSlant>;
+    }
+
+    enum FontSlant {
+        NORMAL,
+        ITALIC,
+        OBLIQUE,
+    }
+
+    export namespace FontWeight {
+        export const $gtype: GObject.GType<FontWeight>;
+    }
+
+    enum FontWeight {
+        NORMAL,
+        BOLD,
+    }
+
+    export namespace SubpixelOrder {
+        export const $gtype: GObject.GType<SubpixelOrder>;
+    }
+
+    enum SubpixelOrder {
+        DEFAULT,
+        RGB,
+        BGR,
+        VRGB,
+        VBGR,
+    }
+
+    export namespace HintStyle {
+        export const $gtype: GObject.GType<HintStyle>;
+    }
+
+    enum HintStyle {
+        DEFAULT,
+        NONE,
+        SLIGHT,
+        MEDIUM,
+        FULL,
+    }
+
+    export namespace HintMetrics {
+        export const $gtype: GObject.GType<HintMetrics>;
+    }
+
+    enum HintMetrics {
+        DEFAULT,
+        OFF,
+        ON,
+    }
+
+    export namespace FontType {
+        export const $gtype: GObject.GType<FontType>;
+    }
+
+    enum FontType {
+        TOY,
+        FT,
+        WIN32,
+        QUARTZ,
+        USER,
+    }
+
+    export namespace PathDataType {
+        export const $gtype: GObject.GType<PathDataType>;
+    }
+
+    enum PathDataType {
+        MOVE_TO,
+        LINE_TO,
+        CURVE_TO,
+        CLOSE_PATH,
+    }
+
+    export namespace DeviceType {
+        export const $gtype: GObject.GType<DeviceType>;
+    }
+
+    enum DeviceType {
+        DRM,
+        GL,
+        SCRIPT,
+        XCB,
+        XLIB,
+        XML,
+        COGL,
+        WIN32,
+        INVALID,
+    }
+
+    export namespace SurfaceType {
+        export const $gtype: GObject.GType<SurfaceType>;
+    }
+
+    enum SurfaceType {
+        IMAGE,
+        PDF,
+        PS,
+        XLIB,
+        XCB,
+        GLITZ,
+        QUARTZ,
+        WIN32,
+        BEOS,
+        DIRECTFB,
+        SVG,
+        OS2,
+        WIN32_PRINTING,
+        QUARTZ_IMAGE,
+        SCRIPT,
+        QT,
+        RECORDING,
+        VG,
+        GL,
+        DRM,
+        TEE,
+        XML,
+        SKIA,
+        SUBSURFACE,
+        COGL,
+    }
+
+    export namespace Format {
+        export const $gtype: GObject.GType<Format>;
+    }
+
+    enum Format {
+        INVALID,
+        ARGB32,
+        RGB24,
+        A8,
+        A1,
+        RGB16_565,
+        RGB30,
+    }
+
+    export namespace PatternType {
+        export const $gtype: GObject.GType<PatternType>;
+    }
+
+    enum PatternType {
+        SOLID,
+        SURFACE,
+        LINEAR,
+        RADIAL,
+        MESH,
+        RASTER_SOURCE,
+    }
+
+    export namespace Extend {
+        export const $gtype: GObject.GType<Extend>;
+    }
+
+    enum Extend {
+        NONE,
+        REPEAT,
+        REFLECT,
+        PAD,
+    }
+
+    export namespace Filter {
+        export const $gtype: GObject.GType<Filter>;
+    }
+
+    enum Filter {
+        FAST,
+        GOOD,
+        BEST,
+        NEAREST,
+        BILINEAR,
+        GAUSSIAN,
+    }
+
+    export namespace RegionOverlap {
+        export const $gtype: GObject.GType<RegionOverlap>;
+    }
+
+    enum RegionOverlap {
+        IN,
+        OUT,
+        PART,
+    }
 
     export class Context extends Cairo.Context {
         constructor(surface: Surface);
